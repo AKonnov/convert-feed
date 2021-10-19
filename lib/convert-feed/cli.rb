16ecbf8b8a85b7eebd75b291d25d587fac10c798
alex_konnov <a.konnov73@bk.ru>
2021-10-19 09:57:24 +0400

    Parsing options in CLI

--- lib/convert-feed/cli.rb
+++ lib/convert-feed/cli.rb
@@ -1,13 +1,51 @@
 # frozen_string_literal: true
 
-# Doc
+require 'optparse'
+
+PROGRAM_NAME = 'convert-feed'
+
 module ConvertFeed
-  # doc
+  # CLI validate and parsed options. Execute command with args
   class CLI
     def run(argv)
-      return false if argv.empty?
+      if argv.empty?
+        print 'Missing arguments, see help for additional info'
+        return true
+      end
 
-      print 'hello'
+      parsed_options = parsed_options(argv)
+      return parsed_options if parsed_options.nil?
+
+      # Executor.new.exec_command(parsed_options, argv.first)
+    end
+
+    private
+
+    def parsed_options(args)
+      parsed_options = { handlers: {} }
+
+      opt_parser = OptionParser.new do |options|
+        options.banner = "Usage: #{PROGRAM_NAME} [options]"
+        options.on('-r', '--[no-]reverse', 'Reverse items') do |r|
+          parsed_options[:handlers][:reverse] = r
+        end
+        options.on('-s', '--[no-]sort', 'Sort items by published date') do |s|
+          parsed_options[:handlers][:sort] = s
+        end
+        options.on('-o', '--out FORMAT', 'Output feed format: atom/rss/xml') do |type|
+          parsed_options[:out_format] = type.downcase
+        end
+        options.on('-f', '--file FILENAME', 'Output into file FILENAME') do |filename|
+          parsed_options[:filename] = filename
+        end
+        options.on('-h', '--help', 'Show this message') do
+          print options
+          return nil
+        end
+      end
+
+      opt_parser.parse!(args)
+      parsed_options
     end
   end
 end
--- test/convert_feed_test.rb
+++ test/convert_feed_test.rb
@@ -5,14 +5,21 @@ require_relative './support/command_helper'
 
 class ConvertFeedTest < Minitest::Test
   include CommandHelper
-  def test_command_success
-    run_command 'convert-feed -f'
-    assert_equal 'hello', last_command.output
+  def test_command_help_success
+    run_command 'convert-feed -h'
+    assert_equal 'Usage: convert-feed [options]
+    -r, --[no-]reverse               Reverse items
+    -s, --[no-]sort                  Sort items by published date
+    -o, --out FORMAT                 Output feed format: atom/rss/xml
+    -f, --file FILENAME              Output into file FILENAME
+    -h, --help                       Show this message
+', last_command.output
     assert_equal 0, last_command.exit_status
   end
 
   def test_cmd_fail_without_args
     run_command 'convert-feed'
+    assert_equal 'Missing arguments, see help for additional info', last_command.output
     assert_equal 1, last_command.exit_status
   end
 end
