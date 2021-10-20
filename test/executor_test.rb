a42f0a8e02dbf324792016b71c39b30a7d0fc46e
alex_konnov <a.konnov73@bk.ru>
2021-10-20 15:51:04 +0400

    Fix fixtures. Change duplicate in rss_not_sorted. Add test_rss_to_atom_sort_to_output

--- test/executor_test.rb
+++ test/executor_test.rb
@@ -7,7 +7,7 @@ require_relative '../lib/executor'
 class ExecutorTest < Minitest::Test
   RSS_FIXTURE_FILEPATH = File.expand_path('fixtures/input_rss.rss', __dir__)
   RSS_REVERSED_FIXTURE_FILEPATH = File.expand_path('fixtures/input_rss.rss', __dir__)
-  RSS_NO_SORT_FIXTURE_FILEPATH = File.expand_path('fixtures/not_sorted.rss', __dir__)
+  RSS_NO_SORT_FIXTURE_FILEPATH = File.expand_path('fixtures/input_rss.not_sorted.rss', __dir__)
   ATOM_REVERSED_FIXTURE_FILEPATH = File.expand_path('../test/fixtures/input_atom.reversed.rss', __dir__)
   ATOM_NO_SORT_FIXTURE_FILEPATH = File.expand_path('../test/fixtures/input_atom.not_sorted.rss', __dir__)
   ATOM_FIXTURE_FILEPATH = File.expand_path('../test/fixtures/input_atom.rss', __dir__)
@@ -75,6 +75,17 @@ class ExecutorTest < Minitest::Test
     end
 
     expected_result = File.read(RSS_FIXTURE_FILEPATH)
+    assert_equal expected_result.chomp, out.chomp
+  end
+
+  def test_rss_to_atom_sort_to_output
+    options = { out: 'atom', sort: true, reverse: true }
+    source = RSS_NO_SORT_FIXTURE_FILEPATH
+    out, err = capture_io do
+      Executor.new.exec_command(options, source)
+    end
+
+    expected_result = File.read(ATOM_FIXTURE_FILEPATH)
     assert_equal expected_result.chomp, out.chomp
   end
 end
